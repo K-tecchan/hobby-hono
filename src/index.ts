f3723a2878ac2075ba98b79bbf903c8dc79b7705
k-tecchan <tecchanboy+pc@gmail.com>
2024-06-22 21:42:03 +0900

    biome init

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,33 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello World!' })
-})
+app.get("/", (c) => {
+	return c.json({ message: "Hello World!" });
+});
 
-app.get('/hello/:id/:name', zValidator('param', z.object({
-  id: z.string().regex(/^\d+$/, "Invalid Id format"),
-  name: z.string().min(1, "Name cannot be empty")
-})), (c) => {
-  const {id, name} = c.req.valid('param')
-  return c.text(`${id} & ${name}`)
-})
+app.get(
+	"/hello/:id/:name",
+	zValidator(
+		"param",
+		z.object({
+			id: z.string().regex(/^\d+$/, "Invalid Id format"),
+			name: z.string().min(1, "Name cannot be empty"),
+		}),
+	),
+	(c) => {
+		const { id, name } = c.req.valid("param");
+		return c.text(`${id} & ${name}`);
+	},
+);
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
-  fetch: app.fetch,
-  port
-})
+	fetch: app.fetch,
+	port,
+});
