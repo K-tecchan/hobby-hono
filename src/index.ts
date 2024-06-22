3cfda5732fe552539791a86d0067ba9ebb42093b
k-tecchan <tecchanboy+pc@gmail.com>
2024-06-22 21:36:24 +0900

    zValidator

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,19 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
 
 const app = new Hono()
-
-const paramSchema = z.object({
-  id: z.string().regex(/^\d+$/, "Invalid Id format"),
-  name: z.string().min(1, "Name cannot be empty")
-})
 
 app.get('/', (c) => {
   return c.json({ message: 'Hello World!' })
 })
 
-app.get('/hello/:id/:name', (c, next) => {
-  const params = c.req.param();
-  const result = paramSchema.safeParse(params)
-  if (!result.success) {
-    return c.json({ error: result.error.errors[0].message}, 400)
-  }
-  return next()
-}, (c) => {
-  const {id, name} = c.req.param()
+app.get('/hello/:id/:name', zValidator('param', z.object({
+  id: z.string().regex(/^\d+$/, "Invalid Id format"),
+  name: z.string().min(1, "Name cannot be empty")
+})), (c) => {
+  const {id, name} = c.req.valid('param')
   return c.text(`${id} & ${name}`)
 })
 
